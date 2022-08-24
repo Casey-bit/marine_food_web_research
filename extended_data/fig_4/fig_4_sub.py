0c093d8b09ae5440425d47987185ea799479ee25
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2022-08-24 13:27:41 +0800

    Update fig_4_sub.py

--- extended_data/fig_4/fig_4_sub.py
+++ extended_data/fig_4/fig_4_sub.py
@@ -1,3 +1,12 @@
+'''
+ # Copyright (C) 2022-08-09 School of IoT, Jiangnan University Limited
+ #
+ # All Rights Reserved.
+ #
+ # Paper: Cascade Shifts of Marine Species in the Northern Hemisphere
+ # First author: Zhenkai Wu
+ # Corresponding author: Ya Guo, E-mail: guoy@jiangnan.edu.cn
+'''
 from netCDF4 import Dataset
 import numpy as np
 import pickle
