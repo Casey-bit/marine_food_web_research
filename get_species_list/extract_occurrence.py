6c27ee5198a8e3db9bd53041f843812534aded1f
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2023-02-09 20:18:52 +0800

    Create extract_occurrence.py

--- get_species_list/extract_occurrence.py
+++ get_species_list/extract_occurrence.py
@@ -1,1 +1,8 @@
+import pandas as pd
 
+data = pd.read_csv('E:\\paper\\obis_20220114.csv\\obis_20220114.csv',usecols=(2,3,16,17,29,32,41,46,51,56,61,83,123))
+data = data.dropna(subset=['eventdate'])
+data = data[data['eventdate'].str[0:4].str.isdigit()]
+
+data_Occurrence = data[data['basisofrecord']=='Occurrence']
+data_Occurrence.to_csv('E:\\paper\\obis_20220114.csv\\data_Occurrence.csv')
