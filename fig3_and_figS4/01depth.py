bbdeb24d27a164c53fa08cde2f826bcbf8abe2d7
Casey-bit <109841863+Casey-bit@users.noreply.github.com>
2023-03-16 12:44:46 +0800

    Update 01depth.py

--- fig3_and_figS4/01depth.py
+++ fig3_and_figS4/01depth.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from statsmodels.formula.api import ols
-from collections import Counter #引入Counter
+from collections import Counter 
 import pickle
 from functools import reduce
 import seaborn as sns
@@ -24,28 +24,22 @@ font2 = {
     'size': 12,
 }
 
-'''
-去除无效数据
-'''
+
 chl_data = pd.read_csv(r'chl_calc\chl_data.csv')
 chl_data.replace('--', np.nan, inplace=True)
 chl_data.dropna(inplace=True)
 chl_data['mean_chl'] = chl_data['mean_chl'].astype(float)
-'''
-保留200米
-'''
+
 # chl_data = chl_data[chl_data['depth'] < 200]
 # chl_data = chl_data[chl_data['year'] >= 2007]
-'''
-求平均
-'''
+
 chl_data_depth_mean = chl_data.groupby(['year','month','depth'])['mean_chl'].mean().reset_index()
 chl_data_depth_year_mean = chl_data_depth_mean.groupby(['year','depth'])['mean_chl'].mean().reset_index()
 
 print(chl_data_depth_year_mean)
 
 '''
-拟合
+regression
 '''
 # g = chl_data_lati_mean.groupby(['latitude'])
 # regr_df = pd.DataFrame({'latitude':[], 'k':[], 'R2':[], 'P':[]})
