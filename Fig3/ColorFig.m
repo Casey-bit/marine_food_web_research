7f9b8007d8afa99fa55b7efb968ff812e282ed1f
Casey-bit <1034190218@stu.jiangnan.edu.cn>
2024-12-04 22:03:49 +0800

    Create ColorFig.m

---
+++ Fig3/ColorFig.m
@@ -0,0 +1,23 @@
+
+function [AdjustedR2]=ColorFig(Lat_C,Lat_level1_mean)
+
+
+    x =Lat_C';
+    y =Lat_level1_mean';
+    years =1993:2020; 
+    numYears = length(unique(years));
+    cmap = jet(numYears);
+    yearIndices = years - min(years) + 1; 
+    [AdjustedR2]=LinearFittingLatAndCha(x,y);
+    hold on
+    scatter(x, y, 50, yearIndices, 'filled'); 
+    colorbar('Ticks', 1:length(unique(years)), ...
+             'TickLabels', unique(years)); 
+    colorbar(gca,'Ticks',[1 8 18 28],'TickLabels',{'1993','2000','2010','2020'});     
+   box on
+    set(gca,'FontName','Arial','FontSize',15,'FontWeight','bold','GridAlpha',0.05);
+    set(gca,'linewidth',2)
+    % ylabel('Latitude(\circN)')
+    % xlabel('Latitude(\circN)')
+    colormap(cmap); 
+end
